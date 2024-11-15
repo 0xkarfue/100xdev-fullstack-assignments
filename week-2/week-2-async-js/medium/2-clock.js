5b5ea2b16da6206fc75381907882ae2f97c7248a
0xkarfue <0xkarfue@gmail.com>
2024-11-15 14:31:51 +0530

    2nd question of the day

--- week-2/week-2-async-js/medium/2-clock.js
+++ week-2/week-2-async-js/medium/2-clock.js
@@ -6,3 +6,19 @@
 //  - HH:MM::SS (Eg. 13:45:23)
 
 //  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+
+setInterval(() => {
+    const now = new Date();
+
+    const options = {
+        hour: '2-digit',     // Format hour with leading zero (01-12)
+        minute: '2-digit',   // Format minute with leading zero (00-59)
+        second: '2-digit',   // Format second with leading zero (00-59)
+        hour12: true         // Use 12-hour format
+    };
+
+    const readableTime = now.toLocaleTimeString('en-US', options);
+
+    console.log(readableTime);
+
+}, 1000)
